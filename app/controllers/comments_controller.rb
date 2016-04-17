37e8a33d5176a1b007a7b2fd6b1dbe88e447dce7
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-04-17 10:20:26 +0200

    Fixed typos in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
      if @comment.save
        format.html {redirect_to @product, notice: 'Review was created successfully. '}
-       format.json {render :show, status: :creatd, location: @product}
+       format.json {render :show, status: :created, location: @product}
      else
        format.html {redirect_to @product, alert: 'Review was not saved successfully.'}
        format.json {render json: @comment.errors, status: :unprocessable_entity}
